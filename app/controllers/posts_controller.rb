e974697df4cd0cc8d66c03441fa361381dfed002
RezeR93 <xael93@gmail.com>
2016-04-06 16:18:19 +0200

    Adesso i post si vedono

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,16 +5,32 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+    
+    respond_to do |format|
+    format.html  # index.html.erb
+    format.json  { render :json => @posts }
+    end
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post = Post.find(params[:id])
+ 
+    respond_to do |format|
+    format.html  # show.html.erb
+    format.json  { render :json => @post }
+    end
   end
 
   # GET /posts/new
   def new
     @post = Post.new
+    
+    respond_to do |format|
+    format.html  # new.html.erb
+    format.json  { render :json => @post }
+    end
   end
 
   # GET /posts/1/edit
